f1234ee6879f10ec28128aa87684d2b7dcf355d3
Marion Londero <maashhh11@gmail.com>
2020-10-28 09:04:06 +0100

    add comments

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,19 +2,75 @@
 
 namespace App\Controller;
 
+use App\Entity\Comments;
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}"), name="app_blog_show"
+     */
+    public function show(Post $post, Request $request, Security $security, EntityManagerInterface $em)
+    {
+        $comment = new Comments();
+
+        $form = $this->createFormBuilder($comment)
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Your Comment',
+            ])
+            ->add('rate', CheckboxType::class, [
+                'attr' => [
+                    'class' => 'form-control my-2',
+                ],
+                'label' => 'Like ?',
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-success',
+                ],
+            ])
+            ->getForm()
+        ;
+        $comment->setUser($security->getUser());
+        $comment->setPost($post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($comment);
+            $em->flush();
+        }
+
+        return $this->render('blog/show.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
         ]);
     }
 }
